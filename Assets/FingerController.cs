35a455784a8e3080580630eed1eefe81edc3123e
Molly <mollymcconaghy@gmail.com>
2024-10-02 11:56:03 +0100

    change movement

--- Assets/FingerController.cs
+++ Assets/FingerController.cs
@@ -2,15 +2,52 @@ using UnityEngine;
 
 public class FingerController : MonoBehaviour
 {
-    public Transform Finger;
-    public float rotationAmount = -2f;
+    public Transform Finger; 
+    public float rotationAmount = -5f;
+
+    private bool isRotatingLeft = false;
+    private bool isRotatingRight = false;
+
+    private void Update()
+    {
+        if (isRotatingLeft)
+        {
+            RotateFingerLeft();
+        }
+
+        if (isRotatingRight)
+        {
+            RotateFingerRight();
+        }
+    }
 
     public void RotateFingerLeft()
     {
-        Finger.Rotate(Vector2.up, rotationAmount);
+        Finger.Rotate(Vector3.up, rotationAmount * Time.deltaTime);
     }
+
     public void RotateFingerRight()
     {
-        Finger.Rotate(Vector2.up, rotationAmount*(-1));
+        Finger.Rotate(Vector3.up, -rotationAmount * Time.deltaTime);
+    }
+
+    public void OnPressFingerLeft()
+    {
+        isRotatingLeft = true;
+    }
+
+    public void OnReleaseFingerLeft()
+    {
+        isRotatingLeft = false;
+    }
+
+    public void OnPressFingerRight()
+    {
+        isRotatingRight = true;
+    }
+
+    public void OnReleaseFingerRight()
+    {
+        isRotatingRight = false;
     }
 }
